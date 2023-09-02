a6138ae63433e8efe3ddf6c07c83c0a221279acd
YongJun <dkfdirvkdnj@gmail.com>
2023-09-02 21:17:05 +0900

    기능 구현 완료

--- src/main/java/com/nhnacademy/mart/Customer.java
+++ src/main/java/com/nhnacademy/mart/Customer.java
@@ -15,21 +15,24 @@ public class Customer {
     }
 
     public void bring(Basket basket) {
-        System.out.println("hihi");
+        System.out.println("NHN 마트에 오신 것을 환영합니다. 사고 싶은 물건을 골라주세요.");
         this.basket = basket;
     }
 
     // TODO pickFoods 메서드 구현
 
     public void pickFoods(FoodStand foodStand) {
-        int amount = 0;
-        for (int i = 0; i < buyList.getItems().size(); i++) {
-            amount += buyList.getItems().get(i).getAmount();
-        }
-        for (int j = 0; j < foodStand.getFoods().size(); j++) {
-            for (int i = 0; i < amount; i++) {
-                if (foodStand.getFoods().get(j).getName().equals(buyList.getItems().get(i).getName())) {
-                    basket.add(foodStand.del(buyList.getItems().get(i).getName(), j));
+//        int amount = 0;
+//        for (int i = 0; i < buyList.getItems().size(); i++) {
+//            amount += buyList.getItems().get(i).getAmount();
+//        }
+        for (int h = 0; h < buyList.getItems().size(); h++) {
+            for (int j = 0; j < foodStand.getFoods().size(); j++) {
+                if (foodStand.getFoods().get(j).getName().equals(buyList.getItems().get(h).getName())) {
+                    for (int i = 0; i < buyList.getItems().get(h).getAmount(); i++) {
+                        basket.add(foodStand.del(buyList.getItems().get(h).getName(), j));
+                    }
+                    break;
                 }
             }
         }
